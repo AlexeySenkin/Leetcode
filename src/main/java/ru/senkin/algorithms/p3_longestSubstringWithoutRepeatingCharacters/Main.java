6397c839def9fdc029e236db419f4ef14ca97b36
alexsenkin <senkinalexey@mail.ru>
2023-10-06 16:11:37 +0300

    modify 06.10.2023

--- src/main/java/ru/senkin/algorithms/p3_longestSubstringWithoutRepeatingCharacters/Main.java
+++ src/main/java/ru/senkin/algorithms/p3_longestSubstringWithoutRepeatingCharacters/Main.java
@@ -4,12 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        //System.out.println(solution.lengthOfLongestSubstring("dvdf"));
-        //System.out.println(solution.lengthOfLongestSubstring("abcabcbb"));
+//        System.out.println(solution.lengthOfLongestSubstring("dvdf"));
+//        System.out.println(solution.lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(solution.lengthOfLongestSubstring("pwwkew"));
 
-        System.out.println(solution.lengthOfLongestSubstring("bbbbb"));
 
-        //System.out.println(solution.lengthOfLongestSubstring("pwwkew"));
+        //System.out.println(solution.lengthOfLongestSubstring("bbbbb"));
+
+
 
     }
 }
